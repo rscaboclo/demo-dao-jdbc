c627f4df5236e6f93b66f0bfa69608cecb3d47be
Ricardo Caboclo <ricardoscaboclo@gmail.com>
2020-05-07 11:06:03 -0300

    DepartmentDao implements

--- src/application/Program.java
+++ src/application/Program.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
 import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
@@ -11,31 +12,74 @@ import model.entities.Seller;
 public class Program {
 
 	public static void main(String[] args) {
+		
+		DepartmentDao depDao = DaoFactory.createDepartmentDao();
+		List<Department> listDep = new ArrayList<>();
+		Department dep = new Department();
 
 		SellerDao sellerDao = DaoFactory.createSellerDao();
-
-		Seller seller = sellerDao.findById(3);
-
-		System.out.println(seller);
+		List<Seller> listSeller = new ArrayList<>();
+		
+		System.out.println("====================Insert====================");
+		dep = new Department(null,"Financeiro");
+		//depDao.insert(dep);
 		
 		System.out.println();
-		Department department = new Department(2, null);
-		List<Seller> list = new ArrayList<>();
-		list = sellerDao.findByDepartment(department);
-		for (Seller obj : list) {
+		System.out.println("====================FindAll====================");
+		listDep = depDao.findAll();
+		for(Department obj : listDep) {
 			System.out.println(obj);
 		}
 
+		System.out.println();
+		System.out.println("====================DeleteByID====================");
+		System.out.println(depDao.findById(7));
+				
+		System.out.println();
+		System.out.println("====================Delete====================");
+		depDao.deleteById(7);
 		
 		System.out.println();
-		List<Seller> listAll = new ArrayList<>();
-		listAll = sellerDao.findAll();
-		for (Seller obj : listAll) {
+		System.out.println("====================FindAll====================");
+		listDep = depDao.findAll();
+		for(Department obj : listDep) {
 			System.out.println(obj);
 		}
 		
+		System.out.println();
+		System.out.println("====================UPDate====================");
+		dep = depDao.findById(4);
+		dep.setName("Food");
+		System.out.println(dep);
+		depDao.update(dep);
 		
 		System.out.println();
+		System.out.println("====================FindAll====================");
+		listDep = depDao.findAll();
+		for(Department obj : listDep) {
+			System.out.println(obj);
+		}
+		//Seller seller = sellerDao.findById(3);
+		//System.out.println(seller);
+		
+		//System.out.println();
+		//Department department = new Department(2, null);
+		
+		//list = sellerDao.findByDepartment(department);
+		//for (Seller obj : list) {
+		//	System.out.println(obj);
+		//}
+
+		
+		//System.out.println();
+		//List<Seller> listAll = new ArrayList<>();
+		//listAll = sellerDao.findAll();
+		//for (Seller obj : listAll) {
+		//	System.out.println(obj);
+		//}
+		
+		
+		//System.out.println();
 		//Seller newSeller = new Seller(null,"Ricardo Santos Caboclo","rscaboclo@gmail.com",new Date(),3500.0,department);
 		//sellerDao.insert(newSeller);
 		
@@ -45,21 +89,21 @@ public class Program {
 		//seller.setName("Maico Santos Caboclo");
 		//sellerDao.update(seller);
 		
-		System.out.println();
-		listAll = new ArrayList<>();
-		listAll = sellerDao.findAll();
-		for (Seller obj : listAll) {
-			System.out.println(obj);
-		}
+		//System.out.println();
+		//listAll = new ArrayList<>();
+		//listAll = sellerDao.findAll();
+		//for (Seller obj : listAll) {
+		//	System.out.println(obj);
+		//}
 		
-		sellerDao.deletById(16);
+		//sellerDao.deletById(16);
 		
-		System.out.println();
-		listAll = new ArrayList<>();
-		listAll = sellerDao.findAll();
-		for (Seller obj : listAll) {
-			System.out.println(obj);
-		}
+		//System.out.println();
+		//listAll = new ArrayList<>();
+		//listAll = sellerDao.findAll();
+		//for (Seller obj : listAll) {
+		//	System.out.println(obj);
+		//}
 
 	}
 
